3c71b79dfa194dade80ce6619735ffe2336d55a8
依然__饭特稀 <707698592@qq.com>
2016-05-18 17:01:00 +0800

    add RxJava and ACache

--- app/src/main/java/com/ep/joy/net/view/MainActivity.java
+++ app/src/main/java/com/ep/joy/net/view/MainActivity.java
@@ -12,8 +12,14 @@ import com.ep.joy.net.http.AppDao;
 import com.ep.joy.net.http.GlideProxy;
 import com.ep.joy.net.http.MyBaseCallBack;
 import com.ep.joy.net.subscribers.ProgressSubscriber;
+import com.jiongbull.jlog.JLog;
 
+import java.util.ArrayList;
 import java.util.List;
+
+import rx.Observable;
+import rx.Subscriber;
+import rx.functions.Func1;
 
 public class MainActivity extends AppCompatActivity {
     private TextView mView;
@@ -21,14 +27,32 @@ public class MainActivity extends AppCompatActivity {
     private static final String baseUrl = "http://tnfs.tngou.net/image";
     int i = 0;
     String mTest;
+    private List<Students> studentsList = new ArrayList<>();
+    private Subscriber<String> sub;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
         mView = (TextView) findViewById(R.id.tv);
         mImageView = (ImageView) findViewById(R.id.img);
+        setData();
+        sub = new Subscriber<String>() {
+            @Override
+            public void onCompleted() {
+
+            }
+
+            @Override
+            public void onError(Throwable e) {
+
+            }
+
+            @Override
+            public void onNext(String s) {
+                JLog.e(s);
+            }
+        };
 
 
     }
@@ -47,7 +71,72 @@ public class MainActivity extends AppCompatActivity {
 
     public void doHttp(View view) {
         // getCar();
-        test();
+        //  test();
+
+        rx();
+
+
+    }
+
+    private void rx() {
+
+        //     Observable.just("abc", "123").subscribe(sub);
+
+//        Observable.from(studentsList).subscribe(new Action1<Students>() {
+//            @Override
+//            public void call(Students students) {
+//                JLog.e(students.getId() + "");
+//            }
+//        });
+
+//        Observable.create(new Observable.OnSubscribe<String>() {
+//            @Override
+//            public void call(Subscriber<? super String> subscriber) {
+//                subscriber.onNext("123");
+//                subscriber.onNext("456");
+//            }
+//        }).subscribe(sub);
+
+//        Observable.from(studentsList).map(new Func1<Students, String>() {
+//            @Override
+//            public String call(Students students) {
+//                return students.getClas()[3];
+//            }
+//        })
+//                .subscribe(sub);
+        Observable.from(studentsList).flatMap(new Func1<Students, Observable<Students.Clas>>() {
+            @Override
+            public Observable<Students.Clas> call(Students students) {
+                return Observable.from(students.clas);
+            }
+        })
+                .subscribe(new Subscriber<Students.Clas>() {
+                    @Override
+                    public void onCompleted() {
+
+                    }
+
+                    @Override
+                    public void onError(Throwable e) {
+
+                    }
+
+                    @Override
+                    public void onNext(Students.Clas clas) {
+                        JLog.e("onNext " + clas);
+                    }
+                });
+
+
+    }
+
+    private void setData() {
+        Students.Clas[] clz = new Students.Clas[3];
+
+        for (int i = 0; i < 10; i++) {
+            Students students = new Students(i, "a" + i, clz);
+            studentsList.add(students);
+        }
 
     }
 
@@ -87,5 +176,51 @@ public class MainActivity extends AppCompatActivity {
                 GlideProxy.getInstance().loadImage(MainActivity.this, baseUrl + result.get(i).getImg(), mImageView);
             }
         });
+    }
+
+
+    class Students {
+        private int id;
+        private String name;
+        private Clas[] clas;
+
+        public Students(int id, String name, Clas[] clas) {
+            this.id = id;
+            this.name = name;
+            this.clas = clas;
+        }
+
+        public class Clas {
+            public String clas;
+
+            public Clas(String clas) {
+                this.clas = clas;
+            }
+        }
+
+
+        public int getId() {
+            return id;
+        }
+
+        public void setId(int id) {
+            this.id = id;
+        }
+
+        public String getName() {
+            return name;
+        }
+
+        public void setName(String name) {
+            this.name = name;
+        }
+
+        public Clas[] getClas() {
+            return clas;
+        }
+
+        public void setClas(Clas[] clas) {
+            this.clas = clas;
+        }
     }
 }
